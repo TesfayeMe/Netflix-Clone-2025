567ff9ed170e2b77e3dafe9bade907104583c62d
TesfayeMe <tesfayemelaku48@gmail.com>
2025-11-04 12:39:19 +0300

    footer  commit

--- netflix-clone/src/Components/Footer/Footer.js
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -1,8 +1,62 @@
 import React from 'react'
-
+import './footer.css';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 function Footer() {
   return (
+    <>
     <div>Footer</div>
+    <div className='footer_outer_container'>
+      <div className='footer_inner_container'>
+<div className='footer_icons'>
+  <FacebookIcon/>
+  <InstagramIcon/>
+  <TwitterIcon/>
+  <YouTubeIcon/>
+ 
+</div>
+<div className='footer_data'>
+  <div>
+    <ul>
+      <li>Audio Description</li>
+      <li>Investor Relations</li>
+      <li>Legal Notices</li>
+    </ul>
+  </div>
+<div>
+    <ul>
+      <li>Help Center</li>
+      <li>Jobs</li>
+      <li>Cookie Preferences</li>
+    </ul>
+</div>
+<div >
+    <ul>
+      <li>Gift Cards</li>
+      <li>Terms of Use</li>
+      <li>Corporate Information</li>
+    </ul>
+</div>
+<div >
+    <ul>
+      <li>Media Center</li>
+      <li>Privacy</li>
+      <li>Contact Us</li>
+    </ul>
+    </div>
+</div>
+<div className='service-code'>
+
+  <p>Service Code</p>
+</div>
+<div className='footer_copyright'>
+  <p>&copy; 1997-2025 Netflix, Inc.</p>
+</div>
+      </div>
+    </div>
+    </>
   )
 }
 
